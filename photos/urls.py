cd8333712356cb0e12119f47e1004b04ac38e8d5
Yoan-Stanislav Antonov Minchev <slaviminchev2@gmail.com>
2025-10-22 08:44:16 +0300

    update urls

--- photos/urls.py
+++ photos/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path,include
+from photos import views
+
+
 urlpatterns = [
+    path('add', views.photo_add_view, name='photos-add'),
+    path('<init:pk>/', include([
+        path('', views.photo_details, name='photos-details'),
+        path('edit/', views.photo_edit_view, name='photos-edit'),]))
 
 ]
