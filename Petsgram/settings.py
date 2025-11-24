b9729dcefc396ae297a12d84e69fbf86dcd1afff
Yoan-Stanislav Antonov Minchev <slaviminchev2@gmail.com>
2025-11-24 23:53:15 +0200

    change settings.py

--- Petsgram/settings.py
+++ Petsgram/settings.py
@@ -16,10 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7dmcjfdq!k#frhd6z!r5b-vg3td_87y#4kz6$-@su3^r^a0n5h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
