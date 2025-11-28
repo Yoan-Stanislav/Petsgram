d9ae639a88e6da0b595fe0df023b6b3dae94660b
Yoan-Stanislav Antonov Minchev <slaviminchev2@gmail.com>
2025-11-28 23:48:28 +0200

    add models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxLengthValidator
 from django.db import models
 from photos.validators import SizeCheck
 # Create your models here.
@@ -6,4 +7,25 @@ class Photos(models.Model):
     photo = models.ImageField(
         validators=[SizeCheck(5)],
 
+    )
+    description = models.TextField(
+        max_length=300,
+        validators=[MaxLengthValidator(10)],
+        blank = True,
+        null = True,
+    )
+
+    location = models.CharField(
+        max_length=30,
+        blank = True,
+        null = True,
+    )
+
+    tagged_pets = models.ManyToManyField(
+        to = "pets.Pet",
+        blank = True,
+    )
+
+    date_of_publication = models.DateField(
+        auto_now = True
     )
