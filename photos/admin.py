a24195b5a86e3289e11e96560c71234a46ebf913
Yoan-Stanislav Antonov Minchev <slaviminchev2@gmail.com>
2025-12-16 23:15:36 +0200

    change admin

--- photos/admin.py
+++ photos/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from photos.models import Photo
 
-
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('id', 'get_tagged_pets', 'date_of_publication', "description")
 
+list_filter = ('date_of_publication',)
     @staticmethod
     def get_tagged_pets(obj:Photo):
         return ','.join([pet.name for pet in obj.tagged_pets.all()])
