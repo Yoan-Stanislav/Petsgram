9e02fd5d2accbf2cd81fd8b4789091785bc07f9b
Yoan-Stanislav <yoan-stanislav@Yoan-Stanislavs-MacBook-Air.local>
2025-12-20 18:59:56 +0200

    fix

--- pets/models.py
+++ pets/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-
-
 class Pet(models.Model):
     name = models.CharField(max_length = 30)
     personal_photo = models.URLField()
