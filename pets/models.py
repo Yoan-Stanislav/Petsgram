2fcab4aeb5d36cec29b74bf21de3b5c7b15f2373
Yoan-Stanislav <yoan-stanislav@Yoan-Stanislavs-MacBook-Air.local>
2025-12-23 23:12:50 +0200

    change pets models

--- pets/models.py
+++ pets/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 class Pet(models.Model):
-    name = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 50)
     personal_photo = models.URLField()
     date_of_birth = models.DateField(
     null = True,
