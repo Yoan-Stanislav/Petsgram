56ba12184e337ba4151606f3d4c96fda553f008f
Yoan-Stanislav <yoan-stanislav@Yoan-Stanislavs-MacBook-Air.local>
2025-12-26 23:11:55 +0200

    fix

--- Petsgram/settings.py
+++ Petsgram/settings.py
@@ -7,9 +7,6 @@ SECRET_KEY = 'django-insecure-7dmcjfdq!k#frhd6z!r5b-vg3td_87y#4kz6$-@su3^r^a0n5h
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
