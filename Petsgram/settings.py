a6c582d5c0fac1cbb9d45dbbad8356a3cd59bc88
Yoan-Stanislav <slaviminchev2@gmail.com>
2026-01-03 23:40:56 +0200

    fix

--- Petsgram/settings.py
+++ Petsgram/settings.py
@@ -97,7 +97,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_DIR = (BASE_DIR / 'static')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
